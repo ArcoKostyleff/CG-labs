3eb84d1ed5140a5aaf206e8757bddb6694bdb797
Alex <alex.zv.ev@gmail.com>
2023-09-27 14:41:02 +0300

    1

--- lab1_var3.cpp
+++ lab1_var3.cpp
@@ -28,53 +28,35 @@ int main() {
     cr1.setPosition({30, 100});
     cr1.setFillColor(sf::Color(0xFF, 0, 0));
 
-    std::deque<sf::CircleShape> traces;
-
-    sf::Vector2f circlePos{};
+    sf::Vector2f circlePos;
 
     sf::Clock timer;
 
-    constexpr float circleSpeedX{30.f};
-    float deltaTime{};
+    constexpr float circleSpeedX = 30.f;
+    float deltaTime;
 
     float radIncr = 1;
     while (window.isOpen()) {
 
-        sf::Event ev;
         window.clear(sf::Color::Blue);
-        window.draw(cr1);
-        while (window.pollEvent(ev)) {
-            if (ev.type == sf::Event::Closed)
-                window.close();
-            if (ev.type == sf::Event::KeyReleased) {
-                if (ev.key.code == sf::Keyboard::Escape) {
-                    gamePause = !gamePause;
-                }
-            }
-        }
-        if (gamePause) {
-            timer.restart();
-        } else {
-            deltaTime = timer.restart().asSeconds();
-            circlePos = cr1.getPosition();
-            circlePos.x += circleSpeedX * deltaTime; // передвижение
-            circlePos.y -= 10 * radIncr *
-                           deltaTime; // чтобы изменение размера шло из центра
-            circlePos.x -= 10 * radIncr *
-                           deltaTime; // чтобы изменение размера шло из центра
-            circleRadius += 10 * radIncr * deltaTime;
 
-            if (circleRadius >= 100 || circleRadius <= 10)
-                radIncr *= -1;
+        deltaTime = timer.restart().asSeconds();
+        circlePos = cr1.getPosition();
+        circlePos.x += circleSpeedX * deltaTime; // передвижение
+        circlePos.y -=
+            10 * radIncr * deltaTime; // чтобы изменение размера шло из центра
+        circlePos.x -=
+            10 * radIncr * deltaTime; // чтобы изменение размера шло из центра
+        circleRadius += 10 * radIncr * deltaTime;
 
-            cr1.setRadius(circleRadius);
-            cr1.setPosition(circlePos);
-        }
+        if (circleRadius >= 100 || circleRadius <= 10)
+            radIncr *= -1;
+
+        cr1.setRadius(circleRadius);
+        cr1.setPosition(circlePos);
 
         window.draw(cr1);
 
         window.display();
     }
 }
-
-
