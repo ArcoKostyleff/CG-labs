71c9dfb8f5b6854f0e1ba3f4570a2a594212a90b
Aptem <maleev2002@yandex.ru>
2023-11-18 19:58:46 +0300

    Lab3

---
+++ lab3/circle.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+/// @brief Рисует дугу окружности в заданную в полярных координатах.
+/// @param window Окно SFML RenderWindow, на котором будет происходить отрисовка.
+/// @param radius Радиус дуги окружности.
+/// @param startAngle Начальный угол дуги в градусах.
+/// @param endAngle Конечный угол дуги в градусах.
+/// @param center Центральная точка дуги.
+void drawPolarCircleArc(sf::RenderWindow &window, float radius, float startAngle, float endAngle, sf::Vector2f center) {
+    // Create an image to draw on
+    sf::Image image;
+    image.create(window.getSize().x, window.getSize().y, sf::Color::Black);
+
+    // Calculate the number of pixels to draw
+    int numPixels = 2 * M_PI * radius * abs(endAngle - startAngle) / 360;
+
+    // Draw each pixel
+    for (int i = 0; i < numPixels; i++) {
+        // Calculate the angle for this pixel
+        float pixelAngle = startAngle + i * ((endAngle - startAngle) / numPixels);
+
+        // Convert to radians
+        pixelAngle = pixelAngle * M_PI / 180;
+
+        // Calculate the position in Cartesian coordinates
+        float x = radius * cos(pixelAngle);
+        float y = radius * sin(pixelAngle);
+
+        // Set the pixel color
+        image.setPixel(center.x + x, center.y + y, sf::Color::White);
+    }
+
+    // Create a texture and sprite from the image
+    sf::Texture texture;
+    texture.loadFromImage(image);
+    sf::Sprite sprite(texture);
+
+    // Draw the sprite
+    window.draw(sprite);
+}
---
+++ lab3/lab3.cpp
@@ -0,0 +1,98 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+
+#include "funcs.h"
+
+struct Polygon {
+    sf::Color color;
+    std::vector<sf::Vector2f> points;
+};
+
+std::vector<Polygon> readPolygonsFromFile(const std::string& filename, int scale = 1, int shiftX = 0, int shiftY = 0) {
+    std::ifstream in(filename);
+    std::vector<Polygon> polygons;
+    if (!in.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return polygons;
+    }
+    // Количество полигонов
+    int polygonsCount;
+    in >> polygonsCount;
+    std::cout << "Polygons count: " << polygonsCount << std::endl;
+    // Считываем полигоны
+    for (int i = 0; i < polygonsCount; i++) {
+        int pointsCount;
+        int r, g, b;
+        Polygon polygon;
+        in >> pointsCount;
+        in >> r >> g >> b;
+        polygon.color.r = r;
+        polygon.color.g = g;
+        polygon.color.b = b;
+        for (int j = 0; j < pointsCount; j++) {
+            float x, y;
+            in >> x >> y;
+            polygon.points.push_back(sf::Vector2f(x * scale + shiftX, y * scale + shiftY));
+        }
+        polygons.push_back(polygon);
+    }
+    in.close();
+    return polygons;
+}
+
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Lab 3 - Variant 1");
+    window.clear(sf::Color::Black);
+    auto polygons = readPolygonsFromFile("/home/aptem/repositories/CG-labs/lab2/pollygons.txt", 30, 400, 300);
+
+    for (auto& polygon : polygons) {
+        std::cout << "Draw pollygon" << std::endl;
+        std::cout << "Points count: " << polygon.points.size() << std::endl;
+        std::cout << "Color: " << (int)polygon.color.r << " " << (int)polygon.color.g << " " << (int)polygon.color.b << std::endl;
+        for (auto& point : polygon.points)
+            std::cout << '(' << point.x << "; " << point.y << ')' << std::endl;
+        std::cout << std::endl;
+
+        drawPolygon(window, polygon.points, polygon.color);
+        auto point = getPointInsidePolygon(polygon.points);
+        floodFill(window, point.x, point.y, polygon.color, polygon.color);
+
+        window.display();
+
+        sleep(1);
+    }
+
+    // // Отрисовка координатной прямой посередине экрана
+    // // Создание вертикальной линии
+    // sf::Vertex verticalLine[] =
+    // {
+    //     sf::Vertex(sf::Vector2f(window.getSize().x / 2, 0), sf::Color::White),
+    //     sf::Vertex(sf::Vector2f(window.getSize().x / 2, window.getSize().y), sf::Color::White)
+    // };
+    // // Создание горизонтальной линии
+    // sf::Vertex horizontalLine[] =
+    // {
+    //     sf::Vertex(sf::Vector2f(0, window.getSize().y / 2), sf::Color::White),
+    //     sf::Vertex(sf::Vector2f(window.getSize().x, window.getSize().y / 2), sf::Color::White)
+    // };
+
+    // window.draw(verticalLine, 2, sf::Lines);
+    // window.draw(horizontalLine, 2, sf::Lines);
+
+    window.display();
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+
+    return 0;
+}
