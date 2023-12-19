80d14d7d3c7342cc7264a218b19bb29107ad5dc7
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2023-12-19 20:29:41 +0300

    Update lab4.cpp

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -251,14 +251,156 @@ Polygon suthHodgClip(const Polygon &poly_points, const Polygon &clipper_points)
 	return newP;
 }
 
+const int LEFT = 1, RIGHT = 2, BOTTOM = 4, TOP = 8;
+int calculateCode(sf::Vector2f& p, sf::Vector2f& min, sf::Vector2f& max)
+{
+    int code = 0;
+    if (p.x < min.x)
+        code |= LEFT;
+    else if (p.x > max.x)
+        code |= RIGHT;
+
+    if (p.y < min.y)
+        code |= BOTTOM;
+    else if (p.y > max.y)
+        code |= TOP;
+
+    return code;
+}
+void cohenSutherland(sf::RenderWindow& window, sf::Vector2f p1,
+    sf::Vector2f p2, sf::Vector2f min, sf::Vector2f max,
+    sf::Color color, bool drawCropped = false)
+{
+
+    int code1 = 0, code2 = 0;
+
+    code1 = calculateCode(p1, min, max);
+    code2 = calculateCode(p2, min, max);
+
+    bool accept = false;
+
+    while (true) {
+        if (code1 == 0 && code2 == 0) {
+            // Обе точки внутри прямоугольника отсечения
+            accept = true;
+            break;
+        }
+        else if (code1 & code2) {
+            // Обе точки снаружи одной из сторон прямоугольника отсечения
+            break;
+        }
+        else {
+            // Определение и отсечение точки снаружи прямоугольника отсечения
+            int codeOut = code1 ? code1 : code2;
+            float x, y;
+
+            if (codeOut & TOP) {
+                // Начало прямой + её длина по Х * расстояние от начала прямой до верха кадрирования / длина прямой по Y
+                // через подобие тругольников считаем где закончится X
+                x = p1.x + (p2.x - p1.x) * (max.y - p1.y) / (p2.y - p1.y);
+                y = max.y;
+            }
+            else if (codeOut & BOTTOM) {
+                x = p1.x + (p2.x - p1.x) * (min.y - p1.y) / (p2.y - p1.y);
+                y = min.y;
+            }
+            else if (codeOut & RIGHT) {
+                y = p1.y + (p2.y - p1.y) * (max.x - p1.x) / (p2.x - p1.x);
+                x = max.x;
+            }
+            else if (codeOut & LEFT) {
+                y = p1.y + (p2.y - p1.y) * (min.x - p1.x) / (p2.x - p1.x);
+                x = min.x;
+            }
+
+            if (codeOut == code1) {
+                if (drawCropped)
+                    drawLineBresenham(window, p1, { x, y }, sf::Color::Red);
+
+                p1.x = x;
+                p1.y = y;
+                code1 = 0;
+
+                code1 = calculateCode(p1, min, max);
+
+            }
+            else {
+                if (drawCropped)
+                    drawLineBresenham(window, { x, y }, p2, sf::Color::Red);
+
+                p2.x = x;
+                p2.y = y;
+                code2 = 0;
+
+                code2 = calculateCode(p2, min, max);
+            }
+        }
+    }
+
+    if (accept)
+        drawLineBresenham(window, p1, p2, color);
+    else if (drawCropped)
+        drawLineBresenham(window, p1, p2, sf::Color::Red);
+}
+
+// Отрисовка многоугольника
+void drawPolygonCropped(sf::RenderWindow& window, std::vector<sf::Vector2f>& points, sf::Vector2f min, sf::Vector2f max, sf::Color color, bool drawCropped = false)
+{
+    // Соединяем точки между собой
+    for (size_t i = 0; i < points.size() - 1; i++) {
+        cohenSutherland(window, points[i], points[i + 1], min, max, color, drawCropped);
+    }
+    // Соединяем первую и последнюю точку
+    cohenSutherland(window, points.back(), points.front(), min, max, color, drawCropped);
+}
+
+
+class Renderer {
+public:
+	void wievA() {
+		window.display();
+	}
+	void wievB() {
+		
+		window.display();
+	}
+	void DLDDA() {
+		drawLineDDA(window, { min.x, min.y }, { max.x, min.y }, sf::Color::Green);
+		drawLineDDA(window, { max.x, min.y }, { max.x, max.y }, sf::Color::Green);
+		drawLineDDA(window, { max.x, max.y }, { min.x, max.y }, sf::Color::Green);
+		drawLineDDA(window, { min.x, max.y }, { min.x, min.y }, sf::Color::Green);
+	}
+	void toggleWievMode() {
+		if (modeA) {
+			modeA = false;
+			wievA();
+		}
+		else {
+			modeA = true;
+			wievB();
+		}
+	}
+	sf::RenderWindow window{ sf::VideoMode{WINDOW_WIDTH, WINDOW_HEIGHT}, "Lab 3 - Variant 1" };
+	Renderer() {
+		window.clear(sf::Color::Black);
+		polygons = readPolygonsFromFile("pollygons.txt", SCALE, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+
+	}
+	std::vector<Polygon> polygons;
+
+private:
+	sf::Vector2f p1{ 0, 0 };
+	sf::Vector2f p2{ 300, 250 };
+	sf::Vector2f min{ 150, 100 };
+	sf::Vector2f max{ 250, 200 };
+	bool modeA = 1;
+};
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Lab 3 - Variant 1");
-    window.clear(sf::Color::Black);
-    auto polygons = readPolygonsFromFile("pollygons.txt", SCALE, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+	Renderer wnd;
 
-
-    for (auto& polygon : polygons) {
+	
+    for (auto& polygon : wnd.polygons) {
         std::cout << "Draw pollygon" << std::endl;
         std::cout << "Points count: " << polygon.points.size() << std::endl;
        // std::cout << "Color: " << (int)polygon.color.r << " " << (int)polygon.color.g << " " << (int)polygon.color.b << std::endl;
@@ -266,42 +408,27 @@ int main() {
             std::cout << '(' << point.x << "; " << point.y << ')' << std::endl;
         std::cout << std::endl;
 
-        drawPolygon(window, polygon.points, polygon.color);
-       // auto point = getPointInsidePolygon(polygon.points);
-       // floodFill(window, point.x, point.y, polygon.color, polygon.color);
-       
-       // std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        drawPolygon(wnd.window, polygon.points, polygon.color);
+		
+		for (int i = 0; i < wnd.polygons.size()-1; i++) {
+			Polygon obrezka = suthHodgClip(wnd.polygons[i], wnd.polygons[i+1]);
+			if (obrezka.points.size())drawPolygon(wnd.window, obrezka.points, sf::Color::Red);
+		}
     }
-	Polygon obrezka= suthHodgClip(polygons[0], polygons[1]);
-	if (obrezka.points.size())drawPolygon(window, obrezka.points, sf::Color::Red);
-	//window.display();
-     // Отрисовка координатной прямой посередине экрана
-     // Создание вертикальной линии
-     sf::Vertex verticalLine[] =
-     {
-         sf::Vertex(sf::Vector2f(window.getSize().x / 2, 0), sf::Color::White),
-         sf::Vertex(sf::Vector2f(window.getSize().x / 2, window.getSize().y), sf::Color::White)
-     };
-     // Создание горизонтальной линии
-     sf::Vertex horizontalLine[] =
-     {
-         sf::Vertex(sf::Vector2f(0, window.getSize().y / 2), sf::Color::White),
-         sf::Vertex(sf::Vector2f(window.getSize().x, window.getSize().y / 2), sf::Color::White)
-     };
+	
 
-     window.draw(verticalLine, 2, sf::Lines);
-     window.draw(horizontalLine, 2, sf::Lines);
+	wnd.window.display();
 
-    window.display();
-
-    while (window.isOpen()) {
+    while (wnd.window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (wnd.window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
-                window.close();
+				wnd.window.close();
             }
         }
     }
 
     return 0;
 }
+
+ 
