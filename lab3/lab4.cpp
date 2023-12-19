4e3407f971d2f1bb05f991c7ba01ffd7494d3b08
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2023-12-20 00:02:20 +0300

    добавил Свиночела

--- lab3/lab4.cpp
+++ lab3/lab4.cpp
@@ -203,6 +203,206 @@ void drawFox(sf::RenderWindow& window, std::vector<Polygon>& polygons, bool draw
 
     window.display();
 }
+
+
+// Returns x-value of point of intersection of two
+// lines
+float x_intersect(float x1, float y1, float x2, float y2,
+	float x3, float y3, float x4, float y4)
+{
+	float num = (x1 * y2 - y1 * x2) * (x3 - x4) -
+		(x1 - x2) * (x3 * y4 - y3 * x4);
+	float den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	return num / den;
+}
+
+// Returns y-value of point of intersection of
+// two lines
+float y_intersect(float x1, float y1, float x2, float y2,
+	float x3, float y3, float x4, float y4)
+{
+	float num = (x1 * y2 - y1 * x2) * (y3 - y4) -
+		(y1 - y2) * (x3 * y4 - y3 * x4);
+	float den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	return num / den;
+}
+
+void clipOutside(Polygon& poly_points,
+	float x1, float y1, float x2, float y2)
+{
+	std::vector<std::pair<float, float>> new_points;
+	int new_poly_size = 0;
+
+	// (ix,iy),(kx,ky) are the co-ordinate values of
+	// the points
+	for (int i = 0; i < poly_points.points.size(); i++)
+	{
+		// i and k form a line in polygon
+		int k = (i + 1) % poly_points.points.size();;
+		float ix = poly_points.points[i].x, iy = poly_points.points[i].y;
+		float kx = poly_points.points[k].x, ky = poly_points.points[k].y;
+
+		// Calculating position of first point
+		// w.r.t. clipper line
+		float i_pos = (x2 - x1) * (iy - y1) - (y2 - y1) * (ix - x1);
+
+		// Calculating position of second point
+		// w.r.t. clipper line
+		float k_pos = (x2 - x1) * (ky - y1) - (y2 - y1) * (kx - x1);
+
+		// Case 1 : Если обе точки внутри то пропускаем
+		if (i_pos < 0 && k_pos < 0)
+		{
+			// ничего не делаем
+
+		}
+
+		// Case 2: если первая снаружи
+		else if (i_pos >= 0 && k_pos < 0)
+		{
+			// добавляем точку пересечения и наружнюю
+			new_points.push_back({ ix,iy });
+
+			float x = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			float y = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			new_points.push_back({ x,y });
+
+		}
+
+		// Case 3: When only second point is outside
+		else if (i_pos < 0 && k_pos >= 0)
+		{
+			//Only point of intersection with edge is added
+			float x = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			float y = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			new_points.push_back({ x,y });
+
+		}
+
+		// Case 4: When both points are outside
+		else
+		{
+			//No points are added
+		}
+	}
+
+	// Copying new points into original array
+	// and changing the no. of vertices
+
+	poly_points.points.clear();
+
+	for (int i = 0; i < new_points.size(); i++)
+	{
+		poly_points.points.push_back(sf::Vector2f(new_points[i].first, new_points[i].second));
+	}
+	//return newPoints;
+}
+
+// This functions clips all the edges w.r.t one clip
+// edge of clipping area
+void clip(Polygon &poly_points,
+	float x1, float y1, float x2, float y2)
+{
+	std::vector<std::pair<float, float>> new_points;
+	int new_poly_size = 0;
+
+	// (ix,iy),(kx,ky) are the co-ordinate values of
+	// the points
+	for (int i = 0; i < poly_points.points.size(); i++)
+	{
+		// i and k form a line in polygon
+		int k = (i + 1) % poly_points.points.size();;
+		float ix = poly_points.points[i].x, iy = poly_points.points[i].y;
+		float kx = poly_points.points[k].x, ky = poly_points.points[k].y;
+
+		// Calculating position of first point
+		// w.r.t. clipper line
+		float i_pos = (x2 - x1) * (iy - y1) - (y2 - y1) * (ix - x1);
+
+		// Calculating position of second point
+		// w.r.t. clipper line
+		float k_pos = (x2 - x1) * (ky - y1) - (y2 - y1) * (kx - x1);
+
+		// Case 1 : When both points are inside
+		if (i_pos < 0 && k_pos < 0)
+		{
+			//Only second point is added
+			new_points.push_back({ kx, ky });
+		
+		}
+
+		// Case 2: When only first point is outside
+		else if (i_pos >= 0 && k_pos < 0)
+		{
+			// Point of intersection with edge
+			// and the second point is added
+			float x = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			float y = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			new_points.push_back({ x,y });
+	
+			new_points.push_back({ kx,ky });
+		}
+
+		// Case 3: When only second point is outside
+		else if (i_pos < 0 && k_pos >= 0)
+		{
+			//Only point of intersection with edge is added
+			float x = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			float y = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+			new_points.push_back({ x,y });
+
+		}
+
+		// Case 4: When both points are outside
+		else
+		{
+			//No points are added
+		}
+	}
+
+	// Copying new points into original array
+	// and changing the no. of vertices
+	
+	//std::cout << "size before clip:" << poly_points.points.size()<<std::endl;
+	poly_points.points.clear();
+	
+	for (int i = 0; i < new_points.size(); i++)
+	{
+		poly_points.points.push_back(sf::Vector2f(new_points[i].first,new_points[i].second));
+	}
+	//std::cout << "size after clip:" << poly_points.points.size() << std::endl;
+
+}
+
+// алгоритм кадрирования
+// Сазерленд Коен
+
+
+// отсечение фигурой внутренней областью
+// Сазерленд Хогман
+// Implements Sutherland–Hodgman algorithm
+Polygon suthHodgClip(const Polygon &poly_points, const Polygon &clipper_points)
+{
+	Polygon newP = poly_points;
+	//i and k are two consecutive indexes
+	for (int i = 0; i < clipper_points.points.size(); i++)
+	{
+		int k = (i + 1) % clipper_points.points.size();
+		
+		// We pass the current array of vertices, it's size
+		// and the end points of the selected clipper line
+		clip(newP,  clipper_points.points[i].x,
+			clipper_points.points[i].y, clipper_points.points[k].x,
+			clipper_points.points[k].y);
+	}
+
+	// Printing vertices of clipped polygon
+	for (int i = 0; i < newP.points.size(); i++)
+		std::cout << '(' << newP.points[i].x <<
+		", " << newP.points[i].y << ") ";
+	return newP;
+}
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Lab4");
